34a137256c8b1ff9ab11b2c76cf48d55abd6a027
zwbetz-gh <zwbetz@gmail.com>
2021-09-15 22:22:14 -0500

    challenges

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # NightwatchJS Code Challenge
 
+## Setup
+
+1. Install NodeJS version 14 or higher
+1. Install project dependencies
+
+        npm install
+
+1. Copy file `.env.sample` to a new file `.env`
+1. Download Google Chromedriver. Make sure it matches your Google Chrome version
+1. Edit file `.env` and set `CHROMEDRIVER_PATH` to the absolute path of your `chromedriver`
+
+## Run
+
+1. Run tests
+
+        npm test
+
+## Challenges
+
+### Do Addition
+
+1. Navigate to <https://sample-calculator-app-v2.netlify.app/>
+1. Enter `1` for **Number 1**
+1. Enter `2` for **Number 2**
+1. Select `+` for **Operation**
+1. Click **Submit**
+1. Assert that **Result** equals `3`
+
+### Required Field
+
+1. Navigate to <https://sample-calculator-app-v2.netlify.app/>
+1. Clear **Number 1**
+1. Click **Submit**
+1. Assert that the error message **Number 1 is required** displays
--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,42 +1,8 @@
 module.exports = {
   'do addition': function (browser) {
-    browser.url(browser.launchUrl);
-    const calculator = browser.page.calculator();
-    calculator.setNumber1('3');
-    calculator.setNumber2('3');
-    calculator.setOperation('+');
-    calculator.clickSubmit();
-    calculator.assertResult('6');
-    browser.end();
+    // YOUR CODE GOES HERE
   },
-  'do subtraction': function (browser) {
-    browser.url(browser.launchUrl);
-    const calculator = browser.page.calculator();
-    calculator.setNumber1('3');
-    calculator.setNumber2('3');
-    calculator.setOperation('-');
-    calculator.clickSubmit();
-    calculator.assertResult('0');
-    browser.end();
+  'required field': function (browser) {
+    // YOUR CODE GOES HERE
   },
-  'do multiplication': function (browser) {
-    browser.url(browser.launchUrl);
-    const calculator = browser.page.calculator();
-    calculator.setNumber1('3');
-    calculator.setNumber2('3');
-    calculator.setOperation('*');
-    calculator.clickSubmit();
-    calculator.assertResult('9');
-    browser.end();
-  },
-  'do division': function (browser) {
-    browser.url(browser.launchUrl);
-    const calculator = browser.page.calculator();
-    calculator.setNumber1('3');
-    calculator.setNumber2('3');
-    calculator.setOperation('/');
-    calculator.clickSubmit();
-    calculator.assertResult('1');
-    browser.end();
-  }
 };
